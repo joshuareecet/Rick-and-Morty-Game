cc2720a4d4d005cf593e14569b8b8316f6c2ecce
j <121765426@users.noreply.github.com>
2025-08-17 22:39:20 +0100

    added function docstrings

--- main.py
+++ main.py
@@ -7,10 +7,23 @@ from searchhandler import fuzzySearchCharacter, searchCharacter, printMatches
 url = "https://rickandmortyapi.com/api"
 
 def fetchDictionaries():
+  """
+  Gets all data from rick and morty api and returns as dictionaries.
+  Returns:
+    characters_dict (dict): A dictionary containing all characters.
+    locations_dict (dict): A dictionary containing all locations.
+    episodes_dict (dict): A dictionary containing all episodes.
+  """
   characters_dict, locations_dict, episodes_dict = getDictionaries(url)
   storeDictionaries(characters_dict,locations_dict,episodes_dict)
+  return characters_dict, locations_dict, episodes_dict
 
 def naiveSearch(searchTerm: str, characters_dict: dict):
+  """
+  Old search algorithm - prints searchList containing strings that searchTerm is substring
+  Returns:
+    None
+  """
   searchList = searchCharacter(searchTerm,characters_dict)
   for items in range(0,len(searchList)):
     print(searchList[items]['name']+": "+str(searchList[items]['id']))
@@ -19,9 +32,11 @@ def main():
   pass
 
 def test():
+  
+  characters_dict, locations_dict, episodes_dict = fetchDictionaries()
 
   #Collecting the dictionaries
-  characters_dict, locations_dict, episodes_dict = readDictionaries()
+  #characters_dict, locations_dict, episodes_dict = readDictionaries()
   
   #Conducting search on character name:
   searchTerm = input("Please enter the character you would like to find: ")
